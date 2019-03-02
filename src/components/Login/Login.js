11adb3db31c2541735a99e3c47efc6e85feed8e8
Ahmed hadj ammar <gommez1991@gmail.com>
2019-03-02 13:38:38 +0100

    sd

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -33,18 +33,17 @@ class Login extends Component {
     render() {
         return(
             <div>
-                < form   action = "/action_page.php" >
                     < div className = "imgcontainer" >
                     < img src = "https://www.w3schools.com/howto/img_avatar2.png"alt = "Avatar" className = "avatar" />
 
                     <div className="container">
                         <label htmlFor="uname"><b>Username</b></label>
-                        <input type="text" placeholder="Enter Username" name="uname" required onchange={event=>this.setState({email:event.target.value})}/>
+                        <input type="text" placeholder="Enter Username" name="uname" required onChange={event=>this.setState({email:event.target.value})}/>
 
                             <label htmlFor="psw"><b>Password</b></label>
-                            <input type="password" placeholder="Enter Password" name="psw" required onchange={event=>this.setState({password:event.target.value})} />
+                            <input type="password" placeholder="Enter Password" name="psw" required onChange={event=>this.setState({password:event.target.value})} />
 
-                            <button type="submit" onClick={this.login.bind(this)}>Login</button>
+                            <button onClick={this.login.bind(this)}>Login</button>
                             <label>
                                 <input type="checkbox" checked="checked" name="remember"/> Remember me
                             </label>
@@ -55,7 +54,6 @@ class Login extends Component {
                         <span className="psw">Forgot <a href="#">password?</a></span>
                     </div>
                     </div>
-                </form>
             </div>
 
         )
